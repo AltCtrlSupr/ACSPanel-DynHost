17eb00de7f080239f7e89cf0fa7ca8be5a80be12
Genar <gtrias@gmail.com>
2013-12-27 22:18:12 +0100

    Fixing routes and declarations

---
+++ Event/Subscribers/DynHostMenuSubscriber.php
@@ -0,0 +1,31 @@
+<?php
+namespace ACS\ACSPanelDynHost\ACSACSPanelDynHostBundle\Event\Subscribers;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use ACS\ACSPanelBundle\Event\FilterMenuEvent;
+
+class DynHostMenuSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'menu.admin.after.items'     => array(
+                array('dynhostItems', 10),
+            ),
+            'menu.reseller.after.items'     => array(
+                array('dynhostItems', 10),
+            ),
+            'menu.user.after.items'     => array(
+                array('dynhostItems', 10),
+            )
+
+        );
+    }
+
+    public function dynhostItems(FilterMenuEvent $menu_filter)
+    {
+        $menu = $menu_filter->getMenu();
+        // TODO: Change the name of settings action
+        $menu->addChild('menu.dynhost.main', array( 'route' => 'acsacs_dynhost_list'));
+    }
+}
